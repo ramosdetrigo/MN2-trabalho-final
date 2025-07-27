3f586b93d97a551a6609c1cc8191c8937b69801a
Ramos-notebook <gu.ramos@proton.me>
2025-07-27 20:01:48 -0300

    Implementei sobel

---
+++ examples/sobel.rs
@@ -0,0 +1,65 @@
+use sdl2::{image::SaveSurface, surface::Surface};
+use trabalho_final::unidade1::imagem::*;
+
+fn main() {
+    let img = load_png("boats.png");
+
+    // Kernel sobel 3x3 (retirado do vídeo)
+    let sobel_x = [
+        vec![-1.0, 0.0, 1.0],
+        vec![-2.0, 0.0, 2.0],
+        vec![-1.0, 0.0, 1.0],
+    ];
+    let sobel_y = [
+        vec![1.0, 2.0, 1.0],
+        vec![0.0, 0.0, 0.0],
+        vec![-1.0, -2.0, -1.0],
+    ];
+
+    // Realizamos uma convolução com os kernels de sobel para pegar as derivadas X e Y
+    let img_a = convolution(&img, &sobel_x); // dx
+    let img_b = convolution(&img, &sobel_y); // dy
+
+    // Imagem C (gradiente da imagem original)
+    let mut img_c = Surface::new(img.width(), img.height(), img.pixel_format_enum()).unwrap();
+
+    // A raiz dos quadrados de cada pixel (clamp entre 0 e 1) nos dá o gradiente
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            let pixel_a = get_pixelf(&img_a, x, y);
+            let pixel_b = get_pixelf(&img_b, x, y);
+
+            // eleva os pixels ao quadrado, soma eles, e tira a raiz
+            let pixel_c = (
+                (pixel_a.0.powi(2) + pixel_b.0.powi(2)).sqrt().min(1.0),
+                (pixel_a.1.powi(2) + pixel_b.1.powi(2)).sqrt().min(1.0),
+                (pixel_a.2.powi(2) + pixel_b.2.powi(2)).sqrt().min(1.0),
+            );
+
+            set_pixelf(&mut img_c, x, y, pixel_c);
+        }
+    }
+
+    // Imagem D (imagem final com threshold)
+    let mut img_d = Surface::new(img.width(), img.height(), img.pixel_format_enum()).unwrap();
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            let pixel_c = get_pixelf(&img_c, x, y);
+
+            // Aplicando um threshold de 0.5 (colore o pixel de preto se estiver abaixo de 0.5, e de branco se acima)
+            // OBS: Como as imagens estão em preto e branco, podemos assumir que
+            // o valor do pixel é qualquer um dos 3 valores (r, g, ou b)
+            if pixel_c.0 < 0.5 {
+                set_pixelf(&mut img_d, x, y, (0.0, 0.0, 0.0));
+            } else {
+                set_pixelf(&mut img_d, x, y, (1.0, 1.0, 1.0));
+            }
+        }
+    }
+
+    // Salvando as imagens
+    img_a.save("outputA.png").unwrap();
+    img_b.save("outputB.png").unwrap();
+    img_c.save("outputC.png").unwrap();
+    img_d.save("outputD.png").unwrap();
+}
--- src/unidade1/imagem.rs
+++ src/unidade1/imagem.rs
@@ -1,29 +1,63 @@
-use sdl2::{image::ImageRWops, rwops::RWops, surface::Surface};
+use sdl2::{image::ImageRWops, pixels::PixelFormatEnum, rwops::RWops, surface::Surface};
 
 pub fn load_png(filename: &str) -> Surface<'static> {
     RWops::from_file(filename, "r").unwrap().load_png().unwrap()
 }
 
-pub fn get_pixel(img: &Surface<'static>, x: u32, y: u32) -> [u8; 3] {
-    let index = (img.width() * y + x) as usize;
+pub fn get_pixelf(img: &Surface<'static>, x: u32, y: u32) -> (f64, f64, f64) {
+    let index = (img.width() * y + x) as usize * 3;
     let pixels = img.without_lock().unwrap();
-    [pixels[index], pixels[index + 1], pixels[index + 2]]
+    (
+        pixels[index] as f64 / 255.0,
+        pixels[index + 1] as f64 / 255.0,
+        pixels[index + 2] as f64 / 255.0,
+    )
 }
 
-pub fn set_pixel(img: &mut Surface<'static>, x: u32, y: u32, color: [u8; 3]) {
-    let index = (img.width() * y + x) as usize;
+pub fn set_pixelf(img: &mut Surface<'static>, x: u32, y: u32, color: (f64, f64, f64)) {
+    let index = (img.width() * y + x) as usize * 3;
     let pixels = img.without_lock_mut().unwrap();
-    pixels[index] = color[0];
-    pixels[index + 1] = color[1];
-    pixels[index + 2] = color[2];
+    pixels[index] = (color.0 * 255.0) as u8;
+    pixels[index + 1] = (color.1 * 255.0) as u8;
+    pixels[index + 2] = (color.2 * 255.0) as u8;
 }
 
-pub fn px_avg(pixel: &[u8]) -> u32 {
-    (pixel[0] as u32 + pixel[1] as u32 + pixel[2] as u32) / 3
-}
+pub fn convolution(image: &Surface<'static>, kernel: &[Vec<f64>]) -> Surface<'static> {
+    let width = image.width();
+    let height = image.height();
+    let kernel_size = kernel.len();
+    let k_center = kernel_size as i32 / 2;
 
-pub fn px_set(pixel: &mut [u8], r: u8, g: u8, b: u8) {
-    pixel[0] = r;
-    pixel[1] = g;
-    pixel[2] = b;
+    let mut output = Surface::new(width, height, PixelFormatEnum::RGB24).unwrap();
+
+    for y in k_center..(height as i32 - k_center) {
+        for x in k_center..(width as i32 - k_center) {
+            let mut r = 0.0;
+            let mut g = 0.0;
+            let mut b = 0.0;
+
+            // Aplica o kernel pro pixel (pega os arredores etc.)
+            for j in 0..kernel_size {
+                for i in 0..kernel_size {
+                    let px = x + i as i32 - k_center;
+                    let py = y + j as i32 - k_center;
+
+                    let (pr, pg, pb) = get_pixelf(image, px as u32, py as u32);
+                    let k = kernel[j][i];
+
+                    r += pr * k;
+                    g += pg * k;
+                    b += pb * k;
+                }
+            }
+
+            set_pixelf(&mut output, x as u32, y as u32, (
+                r.clamp(0.0, 1.0),
+                g.clamp(0.0, 1.0),
+                b.clamp(0.0, 1.0),
+            ));
+        }
+    }
+
+    output
 }
